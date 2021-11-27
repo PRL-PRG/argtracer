c9d099a5dbd75cb043d66e68914e6b3f63bffe9c
Filip Krikava <krikava@gmail.com>
2021-11-27 18:09:18 +0100

    Add origins

--- src/util.h
+++ src/util.h
@@ -4,7 +4,11 @@
 #include <R/Rinternals.h>
 #include <optional>
 #include <string>
+#include <unordered_map>
+
+typedef std::pair<std::string, std::string> FunOrigin;
 
 std::optional<std::string> env_get_name(SEXP);
+void populate_namespace_map(SEXP env, std::unordered_map<SEXP, FunOrigin>& map);
 
 #endif // UTIL_H
