1019c498b60123f1e7cd24518d4a0a3c97624e86
Filip Krikava <krikava@gmail.com>
2021-12-01 17:54:39 +0100

    Fix helper not to remove the entire temp directory

--- tests/testthat/helper-sxpdb.R
+++ tests/testthat/helper-sxpdb.R
@@ -1,6 +1,6 @@
 with_tempdb <- function(name, code, envir=parent.frame()) {
   local_env <- new.env(parent=envir)
-  temp_dir <- tempdir("sxpdb")
+  temp_dir <- tempfile(fileext=".sxpdb")
   db <- sxpdb::open_db(temp_dir)
 
   assign(name, db, envir=local_env)
