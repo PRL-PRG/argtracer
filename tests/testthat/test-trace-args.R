6b4c0bfc975cc8e7b98bbddaba6776dc58e09e11
Filip Krikava <krikava@gmail.com>
2022-01-24 15:38:29 +0100

    Update tests to reflect sxpdb changes

--- tests/testthat/test-trace-args.R
+++ tests/testthat/test-trace-args.R
@@ -8,7 +8,7 @@ test_with_db("test context jumps", {
     expect_equal(ret$result, 2L)
     expect_equal(sxpdb::size_db(db), 3L)
 
-    origins <- do.call(rbind, sxpdb::view_origins_db(db))
+    origins <- sxpdb::view_origins_db(db)
 
     expect_equal(unique(origins$pkg), "testpkg")
     expect_equal(unique(origins$fun), "f_return")
@@ -64,7 +64,7 @@ test_with_db("basic test", {
     expect_equal(ret$result, 3L)
     expect_equal(sxpdb::size_db(db), 3)
 
-    origins <- do.call(rbind, sxpdb::view_origins_db(db))
+    origins <- sxpdb::view_origins_db(db)
 
     expect_equal(unique(origins$pkg), "testpkg")
     expect_equal(unique(origins$fun), "f_simple")
@@ -78,7 +78,7 @@ test_with_db("skip unevaluated promises", {
     expect_equal(ret$result, 4L)
     expect_equal(sxpdb::size_db(db), 3)
 
-    origins <- do.call(rbind, sxpdb::view_origins_db(db))
+    origins <- sxpdb::view_origins_db(db)
 
     expect_equal(unique(origins$pkg), "testpkg")
     expect_equal(unique(origins$fun), "f_simple_2_of_3")
@@ -92,7 +92,7 @@ test_with_db("test varargs", {
     expect_equal(ret$result, c(1L, 2L, 3L))
     expect_equal(sxpdb::size_db(db), 4)
 
-    origins <- do.call(rbind, sxpdb::view_origins_db(db))
+    origins <- sxpdb::view_origins_db(db)
 
     expect_equal(unique(origins$pkg), "testpkg")
     expect_equal(unique(origins$fun), "f_varargs")
@@ -105,7 +105,7 @@ test_with_db("error is OK", {
     expect_equal(ret$status, 1L)
     expect_equal(ret$result, NULL)
 
-    origins <- do.call(rbind, sxpdb::view_origins_db(db))
+    origins <- sxpdb::view_origins_db(db)
 
     expect_equal(sum(origins$fun == "f_error"), 1L)
 })
@@ -116,7 +116,7 @@ test_with_db("warning is OK", {
     expect_equal(ret$status, 0L)
     expect_equal(ret$result, 1L)
 
-    origins <- do.call(rbind, sxpdb::view_origins_db(db))
+    origins <- sxpdb::view_origins_db(db)
 
     # return, x, y
     expect_equal(sum(origins$fun == "f_warning"), 3L)
